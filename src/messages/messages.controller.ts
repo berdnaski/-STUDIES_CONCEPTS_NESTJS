04a81f39acb9f7d219bb2a84cf5a3ae2197ce533
berdnaski <berdnaski1177@gmail.com>
2025-05-13 11:41:12 -0300

    feat: implements @params nestjs

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,7 +8,7 @@ export class MessagesController {
   }
 
   @Get(':id')
-  findOne() {
-    return 'Essa rota retorn UM recado';
+  findOne(@Param('id') id: string) {
+    return `Essa rota retorn o recado ${id}`;
   }
 }
