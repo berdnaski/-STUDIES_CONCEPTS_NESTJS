a93d770f31e60022a8a609934c16b6f367ae3cc5
berdnaski <berdnaski1177@gmail.com>
2025-05-14 13:02:13 -0300

    feat: implements findall with typeorm

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,10 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import type { Message } from './entities/message.entity';
-import type { CreateMessageDto } from './dto/create-message.dto';
-import type { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class MessagesService {
+  constructor(
+    @InjectRepository(Message)
+    private readonly messageRepository: Repository<Message>,
+  ) {}
   private lastId = 1;
   private messages: Message[] = [
     {
@@ -17,12 +23,20 @@ export class MessagesService {
     },
   ];
 
-  findAll() {
-    return this.messages;
+  async findAll() {
+    const messages = await this.messageRepository.find();
+
+    return {
+      messages,
+    };
   }
 
-  findOne(id: number) {
-    const message = this.messages.find(item => item.id === id);
+  async findOne(id: number) {
+    const message = await this.messageRepository.findOne({
+      where: {
+        id,
+      },
+    });
 
     if (message) return message;
 
