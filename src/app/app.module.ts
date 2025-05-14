0eadc52380c76ca1fb2e2c2e3c4e014d91c01d83
berdnaski <berdnaski1177@gmail.com>
2025-05-14 11:00:59 -0300

    chore: implements TypeOrmModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'postgres',
+      database: 'concepts_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
