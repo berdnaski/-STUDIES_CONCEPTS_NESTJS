ad5fe4d960119e994fb1a1a23940bc8ca4206fbe
berdnaski <berdnaski1177@gmail.com>
2025-05-14 15:07:13 -0300

    feat: create person with email unique table

--- src/persons/persons.service.ts
+++ src/persons/persons.service.ts
@@ -1,11 +1,38 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Person } from './entities/person.entity';
+import type { Repository } from 'typeorm';
 
 @Injectable()
 export class PersonsService {
-  create(createPersonDto: CreatePersonDto) {
-    return 'This action adds a new person';
+  constructor(
+    @InjectRepository(Person)
+    private readonly personRepository: Repository<Person>,
+  ) {}
+
+  async create(createPersonDto: CreatePersonDto) {
+    try {
+      const personData = {
+        name: createPersonDto.name,
+        passwordHash: createPersonDto.password,
+        email: createPersonDto.email,
+      };
+
+      const newPerson = this.personRepository.create(personData);
+      await this.personRepository.save(newPerson);
+      return {
+        newPerson,
+      };
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException('E-mail já está cadastrado.');
+      }
+
+      throw err;
+    }
   }
 
   findAll() {
