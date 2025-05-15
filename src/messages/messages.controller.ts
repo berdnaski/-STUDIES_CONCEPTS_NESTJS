57fd6395bc8eb4b732cb4d8986a8a0241c85f656
berdnaski <berdnaski1177@gmail.com>
2025-05-15 10:25:48 -0300

    refactor: PipeIntIdPipes implements global

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -9,16 +9,13 @@ import {
   Patch,
   Post,
   Query,
-  UsePipes,
 } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { ParseIntIdPipe } from 'src/common/pipes/parse-int-id.pipe';
 
 @Controller('messages')
-@UsePipes(ParseIntIdPipe)
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
