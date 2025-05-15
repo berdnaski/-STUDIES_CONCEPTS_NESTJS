9f1b619d8009bd007b460175f103244331ea75e3
berdnaski <berdnaski1177@gmail.com>
2025-05-15 15:01:59 -0300

    feat: added Interceptors - Adding header to HTTP response with NestJS Interceptor

---
+++ src/common/interceptors/add-header.interceptor.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import type {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+export class AddHeaderInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const response = context.switchToHttp().getResponse();
+
+    response.setHeader('X-Custom-Header', 'O Valor do cabeçalho');
+
+    return next.handle();
+  }
+}
