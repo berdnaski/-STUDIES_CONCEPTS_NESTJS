943f2fa6860ce2e077abc47db905ad2b9229ec83
berdnaski <berdnaski1177@gmail.com>
2025-05-21 15:52:44 -0300

    feat: insert .env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { AppService } from './app.service';
 import { MessagesModule } from 'src/messages/messages.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonsModule } from 'src/persons/persons.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'postgres',
-      database: 'concepts_db',
-      autoLoadEntities: true,
-      synchronize: true,
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
+      synchronize: Boolean(process.env.SYNCHRONIZE),
     }),
     MessagesModule,
     PersonsModule,
